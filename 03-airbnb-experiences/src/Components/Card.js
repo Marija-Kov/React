ad71514ad9f01d932f7b58b1ef486ecab613d230
Marija K <kccmrj@outlook.com>
2022-08-30 22:47:18 +0200

    Using  data.map() to convert hard-coded props

--- 03-airbnb-experiences/src/Components/Card.js
+++ 03-airbnb-experiences/src/Components/Card.js
@@ -2,19 +2,19 @@
 import star from '../assets/little-star.png';
 
 export default function Card(props){
-    const {status, image, title, rating, number, price} = props;
+    const {coverImg, title, rating, reviewCount, location, price, openSpots} = props;
     return (
         <div className="card">
-            <div className="status">{status}</div>
-            <img className="exp-img" src={image} alt="swimmer" />
+            {!openSpots && <div className="status">sold out</div>}
+            <img className="exp-img" src={coverImg} alt="swimmer" />
             <div className="card-text">
                 <div className="rating">
                     <span><img className="star" src={star} alt="star" width="14px"/>{rating}</span>
-                    <span className="grey-text">({number}) &middot; USA</span>
+                    <span className="grey-text">({reviewCount}) &middot; {location}</span>
                 </div>
                 <p>{title}</p>
                 <p className="price">
-                    <span className="from">From {price}</span>
+                    <span className="from">From ${price}</span>
                     / person </p>
             </div>
         </div>
--- 03-airbnb-experiences/src/Components/Experiences.js
+++ 03-airbnb-experiences/src/Components/Experiences.js
@@ -1,32 +1,24 @@
-import katie from '../assets/swimmer.png';
-import wedding from "../assets/wedding-photography.png";
-import bike from "../assets/mountain-bike.png";
+import data from '../data.js'
 import Card from './Card';
 
 export default function Experiences(){
+
+    const showCard = data.map(e => {
+        return (
+          <Card
+              coverImg={`./images/${e.coverImg}`}
+              rating={e.stats.rating}
+              reviewCount={e.stats.reviewCount}
+              location={e.location}
+              title={e.title}
+              price={e.price}
+              openSpots={e.openSpots} />  
+        )
+    })
     return (
         <div className="experiences">
-            <Card
-              status="sold out"
-              image={katie}
-              rating="5.0"
-              number="6"
-              title="Life lessons with Katie Zaferes"
-              price="$136" />
-            <Card
-              status="online"
-              image= {wedding}
-              rating="5.0"
-              number="30"
-              title="Learn wedding photography"
-              price="$125" />
-            <Card
-              status="open"
-              image= {bike}
-              rating="4.8"
-              number="2"
-              title="Group Mountain Biking"
-              price="$50" />
+            {showCard}
         </div>
     )
 }
+ 
