4b6ac91f75eb990415aba7ee1b4c3f68e72f3f3a
Marija K <kccmrj@outlook.com>
2022-08-30 22:43:21 +0200

    Create data.js

---
+++ 03-airbnb-experiences/src/data.js
@@ -0,0 +1,44 @@
+
+const data =[
+    {
+        id: 1,
+        title: "Life lessons with Katie Zaferes",
+        description: " ",
+        price: 136,
+        coverImg:"swimmer.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "USA",
+        openSpots: 0,
+    },
+        {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: " ",
+        price: 125,
+        coverImg:"wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+        {
+        id: 1,
+        title: "Group Mountain Biking",
+        description: " ",
+        price: 50,
+        coverImg:"mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 27,
+    }
+]
+
+export default data;
