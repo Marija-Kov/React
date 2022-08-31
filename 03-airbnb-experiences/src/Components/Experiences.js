a0dd6ba2427423a310f217b0d9ead133e43a756b
Marija K <kccmrj@outlook.com>
2022-08-31 12:08:59 +0200

    Cleaned up Component props

--- 03-airbnb-experiences/src/Components/Experiences.js
+++ 03-airbnb-experiences/src/Components/Experiences.js
@@ -6,13 +6,8 @@ export default function Experiences(){
     const showCard = data.map(e => {
         return (
           <Card
-              coverImg={`./images/${e.coverImg}`}
-              rating={e.stats.rating}
-              reviewCount={e.stats.reviewCount}
-              location={e.location}
-              title={e.title}
-              price={e.price}
-              openSpots={e.openSpots} />  
+              key={e.id}
+              {...e} /> 
         )
     })
     return (
