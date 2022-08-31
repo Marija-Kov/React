cd1d05655c7697dd28e5fb8e9c8f207f74d6b4c7
Marija K <kccmrj@outlook.com>
2022-08-31 12:13:15 +0200

    Adapted the Component..
    To the prop cleanup in Experiences.js.
    
    Also added more rendering conditions to the badge (className="status").

--- 03-airbnb-experiences/src/Components/Card.js
+++ 03-airbnb-experiences/src/Components/Card.js
@@ -2,19 +2,24 @@
 import star from '../assets/little-star.png';
 
 export default function Card(props){
-    const {coverImg, title, rating, reviewCount, location, price, openSpots} = props;
+    let badgeText;
+    if(!props.openSpots){
+        badgeText = "sold out"
+    }else if(props.location.toLowerCase()==="online"){
+        badgeText = "online"
+    }
     return (
         <div className="card">
-            {!openSpots && <div className="status">sold out</div>}
-            <img className="exp-img" src={coverImg} alt="swimmer" />
+            {badgeText && <div className="status">{badgeText}</div>}
+            <img className="exp-img" src={`./images/${props.coverImg}`} alt="swimmer" />
             <div className="card-text">
                 <div className="rating">
-                    <span><img className="star" src={star} alt="star" width="14px"/>{rating}</span>
-                    <span className="grey-text">({reviewCount}) &middot; {location}</span>
+                    <span><img className="star" src={star} alt="star" width="14px"/>{props.stats.rating}</span>
+                    <span className="grey-text">({props.stats.reviewCount}) &middot; {props.location}</span>
                 </div>
-                <p>{title}</p>
+                <p>{props.title}</p>
                 <p className="price">
-                    <span className="from">From ${price}</span>
+                    <span className="from">From ${props.price}</span>
                     / person </p>
             </div>
         </div>
