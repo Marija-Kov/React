357c137d056dda002612f08911430d357c85127f
Marija K <kccmrj@outlook.com>
2022-09-08 21:51:31 +0200

    Applying controlled state principle...
    ..to the meme project input elements.

--- 05-meme-generator/src/components/memeForm.js
+++ 05-meme-generator/src/components/memeForm.js
@@ -1,11 +1,11 @@
 import memesData from "../memesData"
 import React from 'react'
 
-export default function memeForm(){
+export default function MemeForm(){
 
     const [meme, setMeme] = React.useState({
-        topText:"...",
-        bottomText:"...",
+        topText:"",
+        bottomText:"",
         memeImage: "https://i.imgflip.com/24y43o.jpg"
     })
 
@@ -19,16 +19,31 @@ export default function memeForm(){
                 memeImage: allMemeImages.data.memes[Math.floor(Math.random()*len)].url
             }
         })
+        document.querySelector('.top-text').innerText ="";
+        document.querySelector('.bottom-text').innerText =""
     }
+   const handleChange = (event) => {
+       const{name, value} = event.target;
+       setMeme(prev => {
+           return {
+               ...prev,
+               [name]: value 
+           }
+       })
+   }
+
+   const handleSubmit = (event) => {
+       event.preventDefault()
+   }
+
 
     return (
         <> 
-        <div className="form" >
-            <input type="text" placeholder="top text" />
-            <input type="text" placeholder="bottom text" />
-            <button onClick={getMemeImage}>Get a new meme image</button>
-          
-        </div>
+        <form className="form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="top text" name="topText" value={meme.topText} onChange={handleChange}/>
+            <input type="text" placeholder="bottom text" name="bottomText" value={meme.bottomText} onChange={handleChange} />
+        </form>
+        <button onClick={getMemeImage}>Get a new meme image</button>
         <div className="output">
             <img className="memeImg" src={meme.memeImage} alt="meme" />
           <div className="meme-text">
