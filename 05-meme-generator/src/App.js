d03c85bdc7043d9c18555256bba0b4d66d5cda15
Marija K <kccmrj@outlook.com>
2022-09-12 09:54:45 +0200

    Readapting to original Meme Generator project

--- 05-meme-generator/src/App.js
+++ 05-meme-generator/src/App.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Header from './components/Header'
-// import MemeForm from './components/MemeForm'
+import MemeForm from './components/MemeForm'
 //import Form from './components/Output'
 //import Starwars from './components/Output'
-import WindowTracker from './components/Output'
+//import WindowTracker from './components/Output'
 import './App.css';
 
 function App() {
@@ -15,10 +15,7 @@ function App() {
   return (
     <div className="container">
      <Header />
-     <button onClick={toggle}>
-        Toggle WindowTracker
-      </button>
-     {show && <WindowTracker />}
+     <MemeForm />
     </div>
   );
 }
