d8776d807fe8d305a0e1e00dfe8db3c5d720f599
Marija K <kccmrj@outlook.com>
2022-09-17 23:25:10 +0200

    Update Die.js
    The user will be able to hold dice  while rolling others and the held dice will be highlighted with colour.

--- 07-tenzies/src/components/Die.js
+++ 07-tenzies/src/components/Die.js
@@ -2,7 +2,7 @@
 export default function Die(props){
 
     return(
-        <div className={`die ${props.isHeld ? 'held' : ""}`}>
+        <div className={`die ${props.isHeld ? 'held' : ""}`} onClick={props.holdDie}>
          <p>{props.value}</p>
         </div>
     )
