8d621cb9a56c7d3d07ea5eb21b00617e80b50a43
Marija K <kccmrj@outlook.com>
2022-09-17 23:22:50 +0200

    Update RollBtn.js
    The button functionality renders conditionally. If the user won, they can start a new game, otherwise they can keep rolling.

--- 07-tenzies/src/components/RollBtn.js
+++ 07-tenzies/src/components/RollBtn.js
@@ -3,8 +3,8 @@
 export default function RollBtn(props){
 
   return (
-      <div className='roll--btn' onClick={props.rollDice}>
-       Roll
+      <div className='roll--btn' onClick={props.tenzies ? props.newGame : props.rollDice}>
+       {props.tenzies ? 'Play again' : 'Roll'}
       </div>
   )
 }
