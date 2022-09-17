bbf9c275033c012812462810f3bd39a1f1435f85
Marija K <kccmrj@outlook.com>
2022-09-17 23:30:10 +0200

    Update App.js

--- 07-tenzies/src/App.js
+++ 07-tenzies/src/App.js
@@ -3,32 +3,84 @@ import React from 'react';
 import{nanoid} from "nanoid"
 import Die from './components/Die'
 import RollBtn from './components/RollBtn'
+import Confetti from 'react-confetti'
 
 function App() {
 const [diceVals, setDiceVals] = React.useState(allNewDiceVals(10,6));
+const [tenzies, setTenzies] = React.useState(false);
 
- function allNewDiceVals(len, max){
+React.useEffect(() => {
+  let held = diceVals.filter(die => die.isHeld);
+  let allSame = diceVals.every(die => die.value === diceVals[0].value);
+if(held.length === diceVals.length && allSame){
+  setTenzies(true)
+}
+}, [diceVals]);
+
+ function newDie(){
+   return {
+      id: nanoid(), 
+      value: Math.ceil(Math.random()*6), 
+      isHeld: false
+   }
+ }
+
+ function allNewDiceVals(len){
  let arr = [];
  for (let i=0; i<len; ++i){
-   arr.push({ id: nanoid(), value: Math.ceil(Math.random()*max), isHeld: false})
+   arr.push(newDie())
  }
  return arr
 } 
 
 function rollDice(){
- setDiceVals(allNewDiceVals(10,6))
+ setDiceVals(prev => prev.map(die => {
+   return (
+     die.isHeld ? die : newDie()
+   )
+ }))
 }
 
-const dice = diceVals.map(dieVal => <Die key={dieVal.id} value={dieVal.value} isHeld={dieVal.isHeld}/>);
+function newGame(){
+  setDiceVals(prev => prev.map(die => newDie()))
+  setTenzies(false)
+}
+
+function holdDie(id){
+  setDiceVals(prev => prev.map(die =>{
+    return (
+      die.id === id ? 
+      {...die, isHeld: !die.isHeld} :
+      die
+    )
+  }))
+  
+}
+
+const dice = diceVals.map(dieVal => {
+  return(
+<Die key={dieVal.id} 
+     value={dieVal.value} 
+     isHeld={dieVal.isHeld}
+     holdDie={()=>holdDie(dieVal.id)}
+     />
+  )
+});
 
   return (
+    <>
     <main>
+      {tenzies && <Confetti />}
+      <div className="description">
+      <h1>Tenzies</h1>
+      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </div>
      <div className="dice--wrapper">
     {dice}
      </div> 
-     <RollBtn rollDice={rollDice} />
+     <RollBtn rollDice={rollDice} tenzies={tenzies} newGame={newGame}/>
     </main>
-    
+   </> 
   );
 }
 
