6bc1088464f210474955a85c6f8f2be4e19ee10c
Marija K <kccmrj@outlook.com>
2022-09-17 14:04:25 +0200

    Update App.js
    - f() allNewDiceVals returns an array of 10 objects with a few properties
    - variable 'dice' holds an array of  component instances ( created using map() ) where each is passed a prop "value" holding a random number 1-6 inclusive
    - every die instance is passed 'isHeld' prop that holds a boolean value
    - A click on RollBtn resets dice values by calling allNew DIceVals function.

--- 07-tenzies/src/App.js
+++ 07-tenzies/src/App.js
@@ -1,10 +1,32 @@
-
 import './App.css';
+import React from 'react';
+import{nanoid} from "nanoid"
+import Die from './components/Die'
+import RollBtn from './components/RollBtn'
 
 function App() {
+const [diceVals, setDiceVals] = React.useState(allNewDiceVals(10,6));
+
+ function allNewDiceVals(len, max){
+ let arr = [];
+ for (let i=0; i<len; ++i){
+   arr.push({ id: nanoid(), value: Math.ceil(Math.random()*max), isHeld: false})
+ }
+ return arr
+} 
+
+function rollDice(){
+ setDiceVals(allNewDiceVals(10,6))
+}
+
+const dice = diceVals.map(dieVal => <Die key={dieVal.id} value={dieVal.value} isHeld={dieVal.isHeld}/>);
+
   return (
     <main>
-      
+     <div className="dice--wrapper">
+    {dice}
+     </div> 
+     <RollBtn rollDice={rollDice} />
     </main>
     
   );
