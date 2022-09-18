c44f00968164dd9804726771a915bf9357b7421b
Marija K <kccmrj@outlook.com>
2022-09-18 18:59:02 +0200

    Update InputBox.js

--- 05-meme-generator/src/components/InputBox.js
+++ 05-meme-generator/src/components/InputBox.js
@@ -1,8 +1,26 @@
 import React from "react"
 
 export default function InputBox(props){
+ const [inputTxt, setInputTxt] = React.useState({
+     inputBox1: ""
+ }); 
 
+  console.log(inputTxt[`inputBox${props.id}`])
+
+  function handleChange(event){
+   const {name, value} = event.target;
+   setInputTxt(prev=>{
+       return {
+           ...prev,
+           [name]:value
+       }
+   })
+   }
     return(
-        <input placeholder={`input box ${props.num}`} name={`inputBox${props.num}`} onChange={props.handleChange}/>
+        <input 
+          type="text" 
+          placeholder={`input box ${props.id}`} 
+          name={`inputBox${props.id}`} 
+          onChange={handleChange}/>
     )
 }
