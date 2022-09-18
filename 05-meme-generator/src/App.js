f4bffc3fa56effe85c13d4b643f595a7e3e15b85
Marija K <kccmrj@outlook.com>
2022-09-18 18:56:37 +0200

    Update App.js

--- 05-meme-generator/src/App.js
+++ 05-meme-generator/src/App.js
@@ -1,9 +1,6 @@
 import React from "react"
 import Header from './components/Header'
 import MemeForm from './components/MemeForm'
-//import Form from './components/Output'
-//import Starwars from './components/Output'
-//import WindowTracker from './components/Output'
 import './App.css';
 
 function App() {
