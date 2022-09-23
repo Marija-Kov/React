57ab44cec3b130445c9f735b0d8ecb7449bbd48b
Marija K <kccmrj@outlook.com>
2022-09-23 10:26:02 +0200

    Traditional dice look - step II
    1. Creating 9 instances of Dot
    2. Passing a pattern fragment(number) to each of the dots as "full" prop (referring to whether the dot should be full /visible or not)

--- 07-tenzies/src/components/Die.js
+++ 07-tenzies/src/components/Die.js
@@ -1,9 +1,21 @@
+import Dot from './Dot'
 
 export default function Die(props){
-
+    function applyPattern(){
+      let areas=[];
+            for(let i=0; i<9; ++i){
+             areas.push(
+              <Dot
+              full={props.pattern[i]} 
+                  />   
+             )
+           }
+         return areas  
+    }
+    const diePattern = applyPattern();
     return(
         <div className={`die ${props.isHeld ? 'held' : ""}`} onClick={props.holdDie}>
-         <p>{props.value}</p>
+         {diePattern}
         </div>
     )
 }
