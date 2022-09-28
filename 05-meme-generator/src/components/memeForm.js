bb369a90a2a1c6f9bda923d666226fbc948ecedf
Marija K <kccmrj@outlook.com>
2022-09-28 11:53:35 +0200

    Update memeForm.js

--- 05-meme-generator/src/components/memeForm.js
+++ 05-meme-generator/src/components/memeForm.js
@@ -9,12 +9,12 @@ export default function MemeForm(){
         .then(data => setAllMemes(data.data.memes))
     },[])
 
-    const [extra, setExtra] = React.useState(0);
     const [allMemes, setAllMemes] = React.useState([]);
     const [meme, setMeme] = React.useState({
         box_count: 1,
         memeImage: "https://i.imgflip.com/24y43o.jpg"
     })
+    const [extra, setExtra] = React.useState(1); // first extra box id is always higher by 1 than the default number of boxes 
     const [inputTxt, setInputTxt] = React.useState({});
     function handleChange(event){
        const {name, value} = event.target;
@@ -27,6 +27,19 @@ export default function MemeForm(){
        
        }
 
+    React.useEffect(()=> {
+        let allInput = document.querySelectorAll('input');
+        allInput.forEach(e => {
+           let eId = e.getAttribute('id');
+           let idNum = eId.slice(8, eId.length+1); // 8 is the index of the 'x' character of the input id, after which comes a number we're looking for
+           if (idNum > meme.box_count){ 
+              //console.log(`hid extra input, num id ${idNum}`) 
+              // Making sure that only default input boxes are shown for the current meme and the extras are hidden...
+               e.setAttribute('class', "hidden");
+           }
+        })
+    }, [meme]); // ...after every meme state change. 
+
     function getMeme() {
        let len = allMemes.length;
        let num = Math.floor(Math.random()*len);
@@ -37,12 +50,7 @@ export default function MemeForm(){
                 memeImage: allMemes[num].url
             }
         }) 
-       setExtra(0);
-
-       // Default input box number === meme.box_count
-       // get every input instance with the id from `inputBox${meme.box_count+1 and over}`
-       // and hide it
-       
+        setExtra(1);
     }
 
    const handleSubmit = (event) => {
@@ -51,20 +59,26 @@ export default function MemeForm(){
 
    function inputBoxes() {
        const boxes = [];
-       for(let i=1; i < meme.box_count+5; ++i){
+       for(let i=1; i < meme.box_count+5; ++i){  // max number of input boxes : meme.box_count+4 (extra number of boxes is arbitrary)
               boxes.push(<InputBox
                           key={`inputBox${i}`} 
                           id={i}
                           handleChange={handleChange}
                           inputTxt={inputTxt}
                           boxCount={meme.box_count}
+                          className={i > meme.box_count ? "hidden" : ""} // moving the ternary expression outside the InputBox component solved the problem of extra boxes not disappearing on get new meme
                           />
                           )
                          } 
        return boxes  
    }
 
-
+   function addInputBox(){
+       let newInputBox = document.querySelector(`#inputBox${meme.box_count+extra}`);
+       newInputBox.classList.remove('hidden');
+       setExtra(prev => ++prev);
+   }
+   //console.log(`last box id: ${extra+1}`)
   
    function outputBoxes(){
        const boxes = [];
@@ -79,15 +93,7 @@ export default function MemeForm(){
                 )
                } 
        return boxes  
-   }
-   function addInputBox(){
-       let newInputBox = document.querySelector(`#inputBox${meme.box_count+extra}`);
-       newInputBox.classList.remove('hidden');
-       setExtra(prev => ++prev);
-   }
-   function hideExtras(){
-       
-   }
+   }    
     return (
         <> 
         <form className="form" onSubmit={handleSubmit}>
