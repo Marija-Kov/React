275376ada694abfc3b21ced03485911f7a554e33
Marija K <kccmrj@outlook.com>
2022-11-13 23:19:33 +0100

    Update TestClass.js

--- 05-meme-generator/src/components/other/TestClass.js
+++ 05-meme-generator/src/components/other/TestClass.js
@@ -19,31 +19,74 @@ class TestApp extends React.Component {
 //   state = {
 //     count: 0,
 //   };
-constructor(){
-    super()
-    this.state = {
-        count: 0
-    }
-    this.change = this.change.bind(this)
+state =  {
+    number: 1,
+    character: {},
+    width: window.innerWidth,
+    showsWidth: false
 }
 
-change(e){
-    this.setState(prev=>{
-        return ({count: prev.count + e})
+useSWAPI=(id)=>{
+    fetch(`https://swapi.dev/api/people/${id}`)
+    .then(res=>res.json())
+    .then(data=> {
+        this.setState(prev=>({
+            character: data
+        }))
     })
 }
 
+changeSize=()=>{
+    this.setState(prev=>({
+        width: window.innerWidth
+    }))
+}
+
+componentDidMount(){
+    console.log("mounted")
+    this.useSWAPI(this.state.number)
+    window.addEventListener("resize", this.changeSize);
+}
+componentDidUpdate(prevProps, prevState) {
+    console.log("updated")
+    if(prevState.number !== this.state.number){
+        this.useSWAPI(this.state.number);
+       }
+}
+
+componentWillUnmount(){
+    console.log("unmounted")
+    window.removeEventListener("resize", this.changeSize)
+}
+   
+
+handleClick = (e) => {
+    this.setState(prev => ({
+    number : prev.number + e
+    }))
+
+}
+
+toggleShowWidth = () => {
+    this.setState(prev=>({
+        showsWidth: !prev.showsWidth
+    }))
+}
+
   render() {
+    console.log("rendered")
     return (
-      <div>
-        <button type="button" onClick={()=>this.change(1)}>
-          add
-        </button>
-        <button type="button" onClick={()=>this.change(-1)}>
-          subtract
-        </button>
-        <p>{this.state.count}</p>
-      </div> 
+      <>
+        <div>
+          <button type="button" onClick={()=>this.handleClick(-1)}>previous</button>
+          <button type="button" onClick={()=>this.handleClick(1)}>next</button>
+          <p>{this.state.number}</p>
+          <p>{this.state.character.name || "Loading..."}</p>
+          <button type="button" onClick={()=>this.toggleShowWidth()}>Show Width</button>
+          {this.state.showsWidth && <p>{this.state.width}</p>}
+        </div>
+        <br></br>
+      </>
     );
   }
 }
